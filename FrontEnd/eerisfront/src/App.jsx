5aead9f59db4aa03cb88b43b9753d47f687456ff
JoseJimenezCaballero <143767138+JoseJimenezCaballero@users.noreply.github.com>
2025-04-14 16:39:43 -0400

    Made design for NavBar
    It does not have functionality yet

--- FrontEnd/eerisfront/src/App.jsx
+++ FrontEnd/eerisfront/src/App.jsx
@@ -1,9 +1,10 @@
 import './styles.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
---
+++ FrontEnd/eerisfront/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+
+function NavBar() {
+  return (
+    <div className="NavBar-container">
+        <div className="NavBar-text">
+            <h3 className="NavBar-title">EERIS Project</h3>
+            <h4 className="NavBar-links">Receipts</h4>
+        </div>
+        <div className="NavBar-LoginBTN">
+            Log In
+        </div>
+
+    </div>
+  );
+}
+
+export default NavBar;
